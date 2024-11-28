c5f5abb0da982006f25624e9b800070a3f249270
Arthur Koehl <avkoehl@ucdavis.edu>
2024-11-28 11:57:14 -0800

    need to do multiple attempts I suppose

--- scripts/dl_1m_dem.py
+++ scripts/dl_1m_dem.py
@@ -1,3 +1,4 @@
+from pathlib import Path 
 import os
 import shutil
 
@@ -10,22 +11,37 @@ ODIR = "../data/catchments/"
 regions = gpd.read_file("../data/all_regions.shp")
 flowlines = gpd.read_file("../data/all_flowlines.shp")
 
-if os.path.exists(ODIR):
-   shutil.rmtree(ODIR)
-os.makedirs(ODIR)
+if not os.path.exists(ODIR):
+   os.makedirs(ODIR)
 
-
+failed_items = []
 for i,(index,row) in enumerate(regions.iterrows()):
     percent = round(i / len(regions) * 100, 2)
-    print(f"processing region {i}, {percent}%, {row['hucID']} {row['cID']}")
+
     full_catchmentID = row['hucID'] + '_' + str(row['cID'])
+
+    if Path(f"{ODIR}/{full_catchmentID}-1m_dem.tif").exists():
+        #print(f"already downloaded {full_catchmentID}")
+        continue
+
+    print(f"processing region {i}, {percent}%, {row['hucID']} {row['cID']}")
+
 
     repro = gpd.GeoSeries(row['geometry'], crs=regions.crs).to_crs("EPSG:4326")
     region = repro.item()
-    dem = py3dep.get_dem(region, crs="EPSG:4326", resolution=1)
-    dem = dem.rio.reproject("EPSG:3310", rescampling=rasterio.enums.Resampling.bilinear)
     flow = flowlines.clip(row['geometry'], keep_geom_type=True, sort=True)
-
-    # save
     flow.to_file(f"{ODIR}/{full_catchmentID}-flowlines.shp")
-    dem.rio.to_raster(f"{ODIR}/{full_catchmentID}-1m_dem.tif")
+
+
+    try:
+        dem = py3dep.get_dem(region, crs="EPSG:4326", resolution=1)
+        dem = dem.rio.reproject("EPSG:3310", 
+            rescampling=rasterio.enums.Resampling.bilinear)
+        dem.rio.to_raster(f"{ODIR}/{full_catchmentID}-1m_dem.tif")
+    except Exception as e:
+        print(f"failed to download {full_catchmentID} exception: {e}")
+        failed_items.append(full_catchmentID)
+
+if len(failed_items):
+   for item in failed_items:
+      print(item)
