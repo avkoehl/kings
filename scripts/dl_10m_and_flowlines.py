46f9eeca81bdb85df903cfefa79a350102f82adb
Arthur Koehl <avkoehl@ucdavis.edu>
2024-11-27 16:44:06 -0800

    better projection handling

--- scripts/dl_10m_and_flowlines.py
+++ scripts/dl_10m_and_flowlines.py
@@ -4,6 +4,7 @@ import json
 from pygeohydro import WBD
 from pynhd import NHD
 import py3dep
+import rasterio
 
 
 # Create base directory for data
@@ -35,6 +36,8 @@ for huc12 in huc12s:
     dem_10m = dem_10m.where(dem_10m > 0)
 
     dem_10m_path = f"{base_dir}/{huc12}-dem_10m.tif"
+    # convert to 3310
+    dem_10m = dem_10m.rio.reproject("EPSG:3310", resampling=rasterio.enums.Resampling.bilinear)
     dem_10m.rio.to_raster(dem_10m_path)
 
 
@@ -42,4 +45,5 @@ for huc12 in huc12s:
     nhd = NHD('flowline_hr')
     flowlines = nhd.bygeom(boundary)
     if not flowlines.empty:
+        flowlines = flowlines.to_crs("EPSG:3310")
         flowlines.to_file(f"{base_dir}/{huc12}-flowlines.shp")
